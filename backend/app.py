b1689aa5aa97c6d25edfdf831052112b1f90b5c4
Harim Hahn <hmh79@cornell.edu>
2023-03-25 16:51:11 -0400

    tagline and genres edited

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 
-# ROOT_PATH for linking with all your files. 
+# ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
+os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 
 # These are the DB credentials for your OWN MySQL
 # Don't worry about the deployment credentials, those are fixed
@@ -16,7 +16,8 @@ MYSQL_USER_PASSWORD = ""
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "kardashiandb"
 
-mysql_engine = MySQLDatabaseHandler(MYSQL_USER,MYSQL_USER_PASSWORD,MYSQL_PORT,MYSQL_DATABASE)
+mysql_engine = MySQLDatabaseHandler(
+    MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
 mysql_engine.load_file_into_db()
@@ -25,29 +26,37 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 CORS(app)
 
-# Sample search, the LIKE operator in this case is hard-coded, 
-# but if you decide to use SQLAlchemy ORM framework, 
+# Sample search, the LIKE operator in this case is hard-coded,
+# but if you decide to use SQLAlchemy ORM framework,
 # there's a much better and cleaner way to do this
+
+
 def sql_search(episode):
     query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
-    keys = ["id","title","descr"]
+    keys = ["id", "title", "descr"]
     data = mysql_engine.query_selector(query_sql)
-    return json.dumps([dict(zip(keys,i)) for i in data])
+    return json.dumps([dict(zip(keys, i)) for i in data])
 
-MOVIEGENRELIST = ["Action", "Adventure", "Comedy", "Horror", "Other"]
+
+MOVIEGENRELIST = ["Action", "Adventure", "Comedy", "Drama",
+                  "Fantasy", "Horror", "Romance", "Sci-fi", "Thriller", "Other"]
+
 
 @app.route("/")
 def home():
     return render_template('base.html', movieGenres=MOVIEGENRELIST)
+
 
 @app.route("/episodes")
 def episodes_search():
     text = request.args.get("title")
     return sql_search(text)
 
+
 @app.route("/sample_output")
 def output():
-    return render_template('output.html',title="sample output")
+    return render_template('output.html', title="sample output")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
