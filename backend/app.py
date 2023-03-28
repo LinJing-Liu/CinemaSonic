0dff2a4c5eb62b48bd0dc40f0066fddd6a414b64
Shiman Zhang <sz383@cornell.edu>
2023-03-27 21:14:15 -0400

    change csv into backend and use simple path

--- backend/app.py
+++ backend/app.py
@@ -16,8 +16,8 @@ print(os.environ['ROOT_PATH'])
 
 # precompute inverted index and idf
 pd.set_option('max_colwidth', 600)
-songs_df = pd.read_csv(os.path.join(os.environ['ROOT_PATH'], "clean_song_dataset.csv"))
-movies_df = pd.read_csv(os.path.join(os.environ['ROOT_PATH'], "clean_movie_dataset.csv"))
+songs_df = pd.read_csv("clean_song_dataset.csv")
+movies_df = pd.read_csv("clean_movie_dataset.csv")
 
 # extract lyrics and movie tokens as list of strings
 songs_df['tokens'] = songs_df["clean lyrics"].apply(eval)
